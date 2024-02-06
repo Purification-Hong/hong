cc3bd352eb43fbe2cbbcd93fa8979c517370fab9
Purification-Hong <158257841+Purification-Hong@users.noreply.github.com>
2024-02-06 14:29:08 +0900

    Add files via upload

---
+++ day07/Ex03_Fibonacci.java
@@ -0,0 +1,11 @@
+package day07;
+/* 피보나치 수열의 숫자 10개를 기억하는 배열을 완성하고 출력  */
+ 
+public class Ex03_Fibonacci {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
---
+++ day07/Ex04_Car.java
@@ -0,0 +1,11 @@
+package day07;
+
+/*  마티즈와 그랜저가 치킨게임을 한다.  마티즈는 1.5km/min으로 진행하고 그랜저는 반대방향에서 2.41km/min 으로 진행한다.
+    과연 두 차량은 언제 사고가 났는지 분을 계산해서 출력. 단, 두 차량의 처음 간격은 10km로 한다  */
+public class Ex04_Car {
+	public static void main(String[] args) {
+		
+
+	}
+
+}
